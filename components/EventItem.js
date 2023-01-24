4933e9140ee02c65edcd8dcc9cba745b96ed03a5
Roshan Shrestha <30362960@students.federation.edu.au>
2023-01-25 10:22:08 +1100

    Front End

--- components/EventItem.js
+++ components/EventItem.js
@@ -9,7 +9,21 @@ export default function EventItem({evt}) {
             <Image src={evt.image ? evt.image : '/images/event-default.png'}
             width={170}
             height={100}
+            alt={evt}
             />
+        </div>
+
+        <div className={styles.info}>
+          <span>
+            {evt.date} at {evt.time}
+          </span>
+          <h3>{evt.name}</h3>
+        </div>
+
+        <div className={styles.link}>
+          <Link href={`/events/${evt.slug}`} legacyBehavior>
+            <a className='btn'>Details</a>
+          </Link>
         </div>
 
     </div>
