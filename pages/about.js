348aca02dbd6819725ce518e6bc3a750090f02ac
Roshan Shrestha <30362960@students.federation.edu.au>
2023-01-24 15:58:09 +1100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/Layout"
+
+export default function AboutPage() {
+  return (
+    <Layout title ='About DJ events'>
+        <h1>About</h1>
+        <p>This is an app to find the latest DJ and other musical events</p>
+        <p> Version: 1.2.4</p>
+      
+    </Layout>
+  )
+}
